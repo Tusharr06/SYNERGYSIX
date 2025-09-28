9c1ca118fcba6cef1dcfe07a6fed7a46a9e21d6e
shreesha <shettyshreesha552@gmail.com>
2025-09-28 18:26:51 +0530

    Update APK: removed old version and added latest build

--- FLUTTER_APP/AgroStick/android/app/src/main/kotlin/com/example/agro_stick/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.agro_stick
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- FLUTTER_APP/AgroStick/lib/secrets.dart
+++
@@ -1,4 +0,0 @@
-// WARNING: Do not commit real secrets in production. Use env/remote config.
-const String geminiApiKey = 'AIzaSyC2atvcWulNQZxdzw3UFOv8NZ-40c9v0tE';
-
-
